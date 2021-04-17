7f5b8f3a59a357b7114ddcddefc5f5b7d1840ac5
knishina <knishina@yahoo-corp.jp>
2021-04-17 10:05:54 +0900

    Add context

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import { SafeArea } from './src/components/utility/safe-area.component'
 import { Text } from 'react-native'
 
 import { Ionicons } from '@expo/vector-icons'
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context'
 
 const Tab = createBottomTabNavigator()
 
@@ -61,19 +62,21 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={createScreenOptions}
-            tabBarOptions={{
-              activeTintColor: 'tomato',
-              inactiveTintColor: 'gray',
-            }}
-          >
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Settings" component={Settings} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={createScreenOptions}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+              }}
+            >
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FlatList } from 'react-native'
 import { Searchbar } from 'react-native-paper'
 import styled from 'styled-components/native'
@@ -6,6 +6,8 @@ import { Spacer } from '../../../components/spacer/spacer.component'
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component'
 
 import { SafeArea } from '../../../components/utility/safe-area.component'
+
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
 
 const SearchContainer = styled.View`
   padding: 16px;
@@ -17,26 +19,23 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar />
-    </SearchContainer>
-    <FlatList
-      data={[
-        { name: 1 },
-        { name: 2 },
-        { name: 3 },
-        { name: 4 },
-        { name: 5 },
-        { name: 6 },
-      ]}
-      renderItem={() => (
-        <Spacer position="bottom" size="large">
-          <RestaurantInfoCard />
-        </Spacer>
-      )}
-      keyExtractor={(item) => item.name}
-    />
-  </SafeArea>
-)
+export const RestaurantsScreen = () => {
+  const restaurantContext = useContext(RestaurantsContext)
+  console.log(restaurantContext)
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <FlatList
+        data={restaurantContext.restaurants}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  )
+}
